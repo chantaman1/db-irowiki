685a4233a64c1d69862c48f9cb2697cc7da6e549
Alex <alex.nelson86@gmail.com>
2023-11-29 12:01:08 -0800

    added the rest of the Monster tables

---
+++ app/Model/MonsterDrop.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MonsterDrop extends Model
+{
+  protected $table = 'monster_Drop';
+    protected $id;
+    protected $type;
+    protected $slot;
+    protected $version;
+    protected $server;
+    protected $item;
+    protected $rate;
+    protected $flag;
+
+
+  protected $fillable=[
+    'id', 'type', 'slot', 'version', 'server', 'item', 'rate', 'flag'
+  ];
+
+  protected $casts = [
+    'id' => 'integer',
+    'type' => 'integer',
+    'slot' => 'integer',
+    'version' => 'integer',
+    'server' => 'integer',
+    'item' => 'integer',
+    'rate' => 'integer',
+    'flag' => 'integer'
+  ];
+}
+
